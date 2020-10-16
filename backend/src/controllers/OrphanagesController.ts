b3c4fb370192a8704e106172561a37b86d350d2a
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-10-16 10:47:36 -0300

    adicionando exception handler

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -3,6 +3,7 @@ import Orphanage from '../models/Orphanage';
 import { Request, Response } from 'express';
 
 import orphanagesView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 export default{
   async index(request:Request, response: Response){
@@ -11,7 +12,7 @@ export default{
     const orphanages = await orphanagesRepository.find({
       relations: ['images']
     });
-    
+
     return response.json(orphanagesView.renderMany(orphanages));
   },
 
@@ -41,8 +42,8 @@ export default{
     const images = requestImages.map(image =>{
       return { path: image.filename }
     })
-  
-    const orphanages = orphanagesRepository.create({ 
+    
+    const data = { 
       name, 
       latitude,
       longitude,
@@ -51,7 +52,26 @@ export default{
       opening_hours,
       open_on_weekends,
       images
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),  
+      opening_hours: Yup.string().required(), 
+      open_on_weekends: Yup.boolean().required(), 
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required()
+      }))
     })
+
+    await schema.validate(data, {
+      abortEarly: false
+    })
+
+    const orphanages = orphanagesRepository.create(data)
   
     await orphanagesRepository.save(orphanages);
   
