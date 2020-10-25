259445516980df01feddad0e976b43be903ce523
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-10-25 11:11:37 -0300

    capturando a posição no mapa do usuário

--- web/src/screens/CreateOrphanage.tsx
+++ web/src/screens/CreateOrphanage.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 
 import { FiPlus } from "react-icons/fi";
 
@@ -18,6 +19,17 @@ const happyMapIcon = L.icon({
 })
 
 export default function CreateOrphanage() {
+  
+  const [position, setPosition] = useState({latitude:0, longitude:0});
+
+  function handleMapClick(event: LeafletMouseEvent){
+    const { lat, lng } = event.latlng;
+
+    setPosition({
+      latitude:lat,
+      longitude: lng
+    })
+  }
 
   return (
     <div id="page-create-orphanage">
@@ -32,12 +44,16 @@ export default function CreateOrphanage() {
               center={[-27.2092052,-49.6401092]} 
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              onclick={handleMapClick}
             >
               <TileLayer 
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+              {position.latitude !== 0 ? 
+              <Marker interactive={false} icon={happyMapIcon} position={[position.latitude,position.longitude]} />
+              : null }
+              
             </Map>
 
             <div className="input-block">
